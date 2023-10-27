1b3afd672f5fc94de3f3f7a44b458ec964aa8bd5
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-10-27 14:06:47 -0500

    Add files via upload

---
+++ src/t3_examen/T3_Examen.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t3_examen;
+
+/**
+ *
+ * @author HP
+ */
+public class T3_Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
